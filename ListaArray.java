b28eab3c38d77c0bdc137f471762461548dd1ffe
Izadora Freitas <109389495+PunkPotatoQueen@users.noreply.github.com>
2022-12-08 00:48:05 +0000

    finalizando primeira parte

--- ListaArray.java
+++ ListaArray.java
@@ -12,6 +12,19 @@ public class ListaArray implements EstruturaDeDados{
         return elementos[chave];
     }
 
+    public void printar (){
+        boolean a = true;
+        int i = 0;
+        while (a){
+            System.out.println(elementos[i] );
+            if (elementos[i+1]==0 && elementos[i+2]==0){
+                a=false;
+            }
+            i++;
+	    }
+    }
+
+
     @Override
     public boolean insert(int chave) {
         if (contador==1000){
@@ -27,7 +40,16 @@ public class ListaArray implements EstruturaDeDados{
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
+        for (int i = 0; i < elementos.length; i++){
+            if (elementos[i] == chave){
+                elementos [i] = 0;
+                for (int j = i; j < elementos.length-1; j++){
+                    elementos[j] = elementos [j+1];
+                }
+                contador = contador-1;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -44,25 +66,49 @@ public class ListaArray implements EstruturaDeDados{
 
     @Override
     public int minimum() {
-        // TODO Auto-generated method stub
+        int parametro = 100000;
+        for (int i = 0; i < elementos.length; i++){
+            if (elementos[i] < parametro){
+                parametro = elementos[i];
+            }
+            if (elementos[i+1]==0 && elementos[i+2]==0){
+                return parametro;
+            }
+        }
         return 0;
     }
 
     @Override
     public int maximum() {
-        // TODO Auto-generated method stub
+        int parametro = 0;
+        for (int i = 0; i < elementos.length; i++){
+            if (elementos[i]>parametro){
+                parametro = elementos[i];
+            }
+            if (elementos[i+1]==0 && elementos[i+2]==0){
+                return parametro;
+            }
+        }
         return 0;
     }
 
     @Override
     public int sucessor(int chave) {
-        // TODO Auto-generated method stub
+         for (int i = 0; i < elementos.length; i++){
+            if (elementos[i] == chave){
+                return elementos[i+1];
+            }
+        }
         return 0;
     }
 
     @Override
     public int prodessor(int chave) {
-        // TODO Auto-generated method stub
+        for (int i = 0; i < elementos.length; i++){
+            if (elementos[i] == chave){
+                return elementos[i-1];
+            }
+        }
         return 0;
     }
 
@@ -72,11 +118,29 @@ public class ListaArray implements EstruturaDeDados{
         r.insert(29);
         r.insert(34);
         r.insert(8);
-        System.out.println(r.get(2));
+        System.out.println(r.get(2) + " esse é o get ");
 
-        System.out.println ("teste");
+        r.printar();
 
+        System.out.println ("mínimo:");
+        System.out.println (r.minimum());
+        System.out.println ("máximo:");
+        System.out.println (r.maximum());
+
+        System.out.println ("resultado da busca:");
         System.out.println (r.search(29));
+
+        System.out.println ("deletando o numero 29:");
+        System.out.println (r.delete(29));
+
+        System.out.println ("sucessor de 34:");
+        System.out.println (r.sucessor(34));
+        
+        System.out.println ("antecessor de 34:");
+        System.out.println (r.prodessor(34));
+
+        r.printar();
+
 
     }
 }
